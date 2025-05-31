2cc367d8a9a9054342764fa157c9263fd8ce67cd
your_username <malahat.m82@gmail.com>
2025-05-31 12:47:35 +0200

    npm test user can navigate to mina böcker

--- src/tests/navigation.spec.js
+++ src/tests/navigation.spec.js
@@ -16,8 +16,19 @@ test.describe('navigation mellan sidor', () => {
 
     const addNewBookButton = page.getByRole('button', { name: 'lägg till ny bok' });
      await expect(addNewBookButton).toBeVisible({ timeout: 500 });
+  });
 
+
+//going to mina böcker
+ test('användaren kan navigera till mina böcker', async ({ page }) => {
+  const myBooksButton = page.getByRole('button', { name: 'mina böcker' });
+  await expect(myBooksButton).toBeEnabled();
+  await myBooksButton.click();
+
+  const emptyMessage = page.getByText(/när du valt, kommer dina favoritböcker att visas här/i);
+  await expect(emptyMessage).toBeVisible({ timeout: 500 });
+});
 
   });
-});
+
 
