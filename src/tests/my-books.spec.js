ac9cc6abbf14cb187e183361d6055c59a61da6c2
your_username <malahat.m82@gmail.com>
2025-06-03 10:36:19 +0200

    npm test when user doesn't choose any book and see a message i page.

--- src/tests/my-books.spec.js
+++ src/tests/my-books.spec.js
@@ -1,1 +1,18 @@
+import { test, expect } from '@playwright/test';
 
+test.describe('mina böcker', () => {
+	//going to project
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://tap-ht24-testverktyg.github.io/exam-template/');
+});
+test ('användaren ser ett meddelande om inga favoritböcker finns(tom)',async({page})=>{
+	//  klicka på mina böcker btn
+	const myBooksButton =page.getByRole('button',{name:'mina böcker'});
+	await expect(myBooksButton).toBeEnabled({timeout:500});
+	await myBooksButton.click();
+ // vänta att meddelandet visas i sidan när det finns inga favoritböcker
+	const defaultMessage=page.getByText(/När du valt, kommer dina favoritböcker att visas här/i);
+	await expect(defaultMessage).toBeVisible({timeout:500});
+
+});
+});
